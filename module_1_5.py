d9716216435f6412bba486e0ba5d69a66fa82a5d
Elvir116 <ter2909@yandex.ru>
2024-08-19 21:42:22 +0300

    commit3

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = (1,2,3, "a", "b", "c", [2, 3.5, 7.6], (3, 'ab'))
 print(immutable_var)
-immutable_var[5] = 10
+immutable_var [5] = 10
 print(immutable_var[5])
 mutable_list = [10,15, "a", "d"]
 mutable_list[0] = "dog"
