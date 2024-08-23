ba5d7a745f57fc552377a64b90d0a0594a20e900
Elvir116 <ter2909@yandex.ru>
2024-08-23 11:04:51 +0300

    commit5

---
+++ module_1_6.py
@@ -0,0 +1,20 @@
+my_dict = {"Ilnar": 2014, "Alsu": 1987, "Emilija": 2019}
+print(my_dict)
+print(my_dict["Alsu"])
+print(my_dict.get("Elvir"))
+my_dict.update({"Iljas": 2009, "Elvir": 1981})
+my_dict ["Elvir"] = 1981
+my_dict ["Iljas"] = 2009
+del my_dict["Elvir"]
+print(my_dict)
+my_set = {"Elvir", 4, 17.5}
+print(my_set)
+my_set.add("a")
+print(my_set)
+my_set.add("b")
+print(my_set)
+my_set.discard(4)
+print(my_set)
+
+
+
